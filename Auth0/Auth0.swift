615ae51bbe3d4649ec34884ace79aec014bc179f
Victor <bonjour@victor.id>
2025-09-14 02:38:19 +0200

    offline_access by default

--- Auth0/Auth0.swift
+++ Auth0/Auth0.swift
@@ -30,7 +30,7 @@ public typealias CredentialsManagerResult<T> = Result<T, CredentialsManagerError
 /**
  Default scope value used across Auth0.swift. Equals to `openid profile email`.
  */
-public let defaultScope = "openid profile email"
+public let defaultScope = "openid profile email offline_access"
 
 /**
  [Authentication API](https://auth0.com/docs/api/authentication) client to authenticate a user with Database, Social,
